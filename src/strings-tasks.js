ab0e5fe1d9fd134829e992fbb538950424ecd745
skayer81 <114329023+skayer81@users.noreply.github.com>
2024-12-16 04:04:59 +0300

    Update strings-tasks.js

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,7 +19,7 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(value) x{
+function getStringLength(value) {
   return value ? value.length : 0;
 }
 
